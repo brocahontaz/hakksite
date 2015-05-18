cac8366be2eec12cbe4ef089706b923d16e8fd72
Johan Andersson <900304@gmail.com>
2015-05-18 23:43:54 +0200

    Initial push.

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>
+			hakk
+		</title>
+		<link type="text/css" rel="stylesheet" href="css/stylesheet.css" />
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+	</head>
+	<body>
+		<div id="top_border_container">
+		</div>
+		<div id="menu_container">
+			<div id="menu_content">
+				<div id="logo_box">
+				</div>
+				<a href="#home" class="menuobject">HOME</a>
+				<a href="#home" class="menuobject">ABOUT</a>
+				<a href="#home" class="menuobject">DOWNLOADS</a>
+				<a href="#home" class="menuobject">GALLERY</a>
+				<a href="#home" class="menuobject">CONTACT</a>
+			</div>
+		</div>
+		<div id="main_wrapper">
+			<div id="main_container">
+				<div id="main_image">
+					<div id="image_overlay_one">
+						<h2>Hakk n' slash wonder for up to 4 players</h2>
+						<p class="breadtext">
+							Set up your own server, or join others in a ferocious game of sword fighting mixed with platformer action, in this muliplayer game for up to 4 individual players simultaniously.
+						</p>
+					</div>
+					<div id="image_overlay_two">
+						<h2 white>DOWNLOAD NOW</h2>
+					</div>
+				</div>
+			</div>	
+		</div>
+		<div id="footer_container">
+		</div>
+	</body>
+</html>
